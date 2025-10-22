e78f0a087c5fcab5c24824b0fe8ce796f57403f0
edwcmiller3 <edwcmiller3@outlook.com>
2025-10-21 20:52:22 -0400

    flesh out directory function and qr code create code

--- src/qr_generator.py
+++ src/qr_generator.py
@@ -1,5 +1,6 @@
 import qrcode
 from PIL.Image import Image
+from datetime import datetime
 import argparse
 import os
 
@@ -11,22 +12,27 @@ import os
 # test.show()
 
 
-def _create_img_dir() -> None:
+def _create_img_dir() -> str:
     current_dir: str = os.getcwd()
     img_dir: str = 'img/'
     full_dir: str = os.path.join(current_dir, img_dir)
 
-    qr_dir: None = os.makedirs(
-        os.path.dirname(full_dir), exist_ok=True)
+    os.makedirs(os.path.dirname(full_dir), exist_ok=True)
+
+    return full_dir
 
 
-def create_qr_code(data: str) -> Image:
-    # TODO
-    return
+def create_qr_code(data: str, path: str) -> Image:
+    qr = qrcode.make(data)
+    ext = '.png'
+    file_name: str = ext.join(datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    qr.save(file_name)
 
 
 def show_qr_code(img: Image) -> None:
     img.show()
+
 
 def main():
     print(_create_img_dir())
